ae15ccc155dd351583dcbb2905cc3ca19852c5dd
Prollad Mondal <prolladmail@gmail.com>
2024-06-20 20:35:37 +0530

    Update huuuge-casino.js

--- index-js/huuuge-casino.js
+++ index-js/huuuge-casino.js
@@ -1,22 +1,33 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const path = require('path');
 
 const url = 'https://mosttechs.com/huuuge-casino-free-coins/';
 
-axios.get(url).then(({ data }) => {
-  const $ = cheerio.load(data);
-  const links = [];
+axios.get(url)
+  .then(({ data }) => {
+    const $ = cheerio.load(data);
+    const links = [];
 
-  $('a[href*="2rc7.app.link"]').each((index, element) => {
-    const link = $(element).attr('href');
-    const text = $(element).text().trim();
-    links.push({ href: link, text: text });
+    $('a[href*="2rc7.app.link"]').each((index, element) => {
+      const link = $(element).attr('href');
+      const text = $(element).text().trim();
+      links.push({ href: link, text: text });
+    });
+
+    console.log('Fetched links:', links);
+
+    const dir = 'links-json';
+    if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+    }
+
+    const filePath = path.join(dir, 'huuuge-casino.json');
+    fs.writeFileSync(filePath, JSON.stringify(links, null, 2), 'utf8');
+    console.log(`Links saved to ${filePath}`);
+  })
+  .catch(err => {
+    console.error('Error fetching links:', err);
+    process.exit(1);
   });
-
-  console.log('Fetched links:', links);
-  fs.writeFileSync('links-json/huuuge-casino.json', JSON.stringify(links, null, 2));
-}).catch(err => {
-  console.error('Error fetching links:', err);
-  process.exit(1); // Exit with an error code
-});
