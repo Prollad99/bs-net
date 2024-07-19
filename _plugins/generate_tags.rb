9aca4e92b0960255eb7970b6450158d29c77ec44
Prollad Mondal <prolladmail@gmail.com>
2024-07-19 15:48:24 +0530

    Update generate_tags.rb

--- _plugins/generate_tags.rb
+++ _plugins/generate_tags.rb
@@ -1,29 +1,38 @@
+# _plugins/generate_tags.rb
 module Jekyll
-  class TagPageGenerator < Generator
-    safe true
-
-    def generate(site)
-      if site.layouts.key? 'tag'
-        site.tags.each do |tag, posts|
-          formatted_tag = tag.downcase.gsub(' ', '-')
-          site.pages << TagPage.new(site, site.source, File.join('tag', formatted_tag), tag)
-        end
-      end
-    end
-  end
-
   class TagPage < Page
-    def initialize(site, base, dir, tag)
+    def initialize(site, base, dir, tag, formatted_tag)
       @site = site
       @base = base
-      @dir  = dir
+      @dir = dir
       @name = 'index.html'
 
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tag.html')
       self.data['tag'] = tag
-      self.data['formatted_tag'] = tag.downcase.gsub(' ', '-')
-      self.data['title'] = "Posts Tagged with \"#{tag}\""
+      self.data['formatted_tag'] = formatted_tag
+      self.data['title'] = "Posts tagged with '#{tag}'"
+    end
+  end
+
+  class TagPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      if site.layouts.key? 'tag'
+        dir = 'tag'
+        site.tags.each_key do |tag|
+          formatted_tag = tag.downcase.gsub(' ', '-')
+          write_tag_page(site, File.join(dir, formatted_tag), tag, formatted_tag)
+        end
+      end
+    end
+
+    def write_tag_page(site, dir, tag, formatted_tag)
+      index = TagPage.new(site, site.source, dir, tag, formatted_tag)
+      index.render(site.layouts, site.site_payload)
+      index.write(site.dest)
+      site.pages << index
     end
   end
 end
