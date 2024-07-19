47dee0eabf0b70e64b0ac42fdf03f240ea62e965
Prollad Mondal <prolladmail@gmail.com>
2024-07-19 19:20:53 +0530

    Update generate_tags.rb

--- _plugins/generate_tags.rb
+++ _plugins/generate_tags.rb
@@ -1,34 +1,27 @@
 module Jekyll
-  class TagPage < Page
-    def initialize(site, base, dir, tag)
-      @site = site
-      @base = base
-      @dir = dir
-      @name = 'index.html'
-
-      self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'tag.html')
-      self.data['tag'] = tag
-      self.data['title'] = "Posts tagged with \"#{tag}\""
-    end
-  end
-
   class TagPageGenerator < Generator
     safe true
 
     def generate(site)
       if site.layouts.key? 'tag'
-        dir = 'tag'
-        site.tags.each_key do |tag|
-          site.pages << TagPage.new(site, site.source, File.join(dir, tag.to_slug), tag)
+        site.tags.each do |tag, posts|
+          site.pages << TagPage.new(site, site.source, File.join('tag', tag), tag)
         end
       end
     end
   end
-end
 
-class String
-  def to_slug
-    self.strip.downcase.gsub(' ', '-').gsub(/[^\w-]/, '')
+  class TagPage < Page
+    def initialize(site, base, dir, tag)
+      @site = site
+      @base = base
+      @dir  = dir
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tag.html')
+      self.data['tag'] = tag
+      self.data['title'] = "Posts Tagged with \"#{tag}\""
+    end
   end
 end
