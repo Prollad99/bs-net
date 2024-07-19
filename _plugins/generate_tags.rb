8e933b3f554bbb5bda73261f019276eae66fcf43
Prollad Mondal <prolladmail@gmail.com>
2024-07-19 15:12:34 +0530

    Update generate_tags.rb

--- _plugins/generate_tags.rb
+++ _plugins/generate_tags.rb
@@ -7,14 +7,14 @@ module Jekyll
         site.tags.each do |tag, posts|
           # Convert tag to lowercase and replace spaces with hyphens
           formatted_tag = tag.downcase.gsub(' ', '-')
-          site.pages << TagPage.new(site, site.source, File.join('tag', formatted_tag), tag)
+          site.pages << TagPage.new(site, site.source, File.join('tag', formatted_tag), formatted_tag, tag)
         end
       end
     end
   end
 
   class TagPage < Page
-    def initialize(site, base, dir, tag)
+    def initialize(site, base, dir, formatted_tag, original_tag)
       @site = site
       @base = base
       @dir  = dir
@@ -22,9 +22,9 @@ module Jekyll
 
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tag.html')
-      self.data['tag'] = tag
-      self.data['formatted_tag'] = tag.downcase.gsub(' ', '-')
-      self.data['title'] = "Posts Tagged with \"#{tag}\""
+      self.data['tag'] = original_tag
+      self.data['formatted_tag'] = formatted_tag
+      self.data['title'] = "Posts Tagged with \"#{original_tag}\""
     end
   end
 end
