90c570b5c39837bf82e0e7326d0a0124355ecae0
Prollad Mondal <prolladmail@gmail.com>
2024-07-20 23:02:03 +0530

    Update update_last_modified.rb

--- _plugins/update_last_modified.rb
+++ _plugins/update_last_modified.rb
@@ -1,18 +1,23 @@
-# _plugins/update_last_modified.rb
 module Jekyll
   class Post
     def update_last_modified_at
+      # Get the modification time of the post file
+      post_mod_time = File.mtime(self.path)
+
       # Get the include files referenced in the content
       includes = self.content.scan(/{% include (.+?) %}/).flatten
-      
+
       # Get the modification times of the include files
       include_file_mod_times = includes.map do |include_file|
         include_path = File.join(@site.in_source_dir("_includes", include_file))
         File.mtime(include_path) if File.exist?(include_path)
       end
 
-      # Update last_modified_at to the latest include file modification time
-      latest_mod_time = include_file_mod_times.compact.max
+      # Combine post and include file modification times
+      all_mod_times = include_file_mod_times.compact << post_mod_time
+
+      # Update last_modified_at to the latest modification time
+      latest_mod_time = all_mod_times.max
       self.data['last_modified_at'] = latest_mod_time if latest_mod_time
     end
   end
