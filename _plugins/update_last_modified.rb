70399447812964f982ea3cc6e45d3a2e114c4bcb
Prollad Mondal <prolladmail@gmail.com>
2024-07-21 08:47:41 +0530

    Update update_last_modified.rb

--- _plugins/update_last_modified.rb
+++ _plugins/update_last_modified.rb
@@ -1,45 +1,24 @@
 module Jekyll
-  class Post
+  module LastModified
     def update_last_modified_at
-      # Get the modification time of the post file
       post_mod_time = File.mtime(self.path)
 
-      # Get the include files referenced in the content
       includes = self.content.scan(/{% include (.+?) %}/).flatten
 
-      # Get the modification times of the include files
       include_file_mod_times = includes.map do |include_file|
-        include_path = File.join(@site.in_source_dir("_includes", include_file))
-        if File.exist?(include_path)
-          File.mtime(include_path)
-        end
+        include_path = @site.in_source_dir("_includes", include_file)
+        File.exist?(include_path) ? File.mtime(include_path) : nil
       end
 
-      # Combine post and include file modification times
       all_mod_times = include_file_mod_times.compact << post_mod_time
-
-      # Update last_modified_at to the latest modification time
       latest_mod_time = all_mod_times.max
+
       self.data['last_modified_at'] = latest_mod_time if latest_mod_time
     end
   end
 
-  class PostCollection
-    def initialize(site)
-      @site = site
-      @posts = []
-      @site.posts.docs.each do |post|
-        post.update_last_modified_at
-        @posts << post
-      end
-    end
-  end
-
-  class LastModifiedGenerator < Generator
-    priority :low
-
-    def generate(site)
-      PostCollection.new(site)
-    end
+  Hooks.register :documents, :pre_render do |document|
+    document.extend LastModified
+    document.update_last_modified_at
   end
 end
