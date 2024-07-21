0d8b9acae5b7e9eabfbb61aff803f4d3064f87db
Prollad Mondal <prolladmail@gmail.com>
2024-07-21 08:39:03 +0530

    Update update_last_modified.rb

--- _plugins/update_last_modified.rb
+++ _plugins/update_last_modified.rb
@@ -3,19 +3,15 @@ module Jekyll
     def update_last_modified_at
       # Get the modification time of the post file
       post_mod_time = File.mtime(self.path)
-      Jekyll.logger.info "Post mod time: ", post_mod_time
 
       # Get the include files referenced in the content
       includes = self.content.scan(/{% include (.+?) %}/).flatten
-      Jekyll.logger.info "Includes: ", includes.join(', ')
 
       # Get the modification times of the include files
       include_file_mod_times = includes.map do |include_file|
         include_path = File.join(@site.in_source_dir("_includes", include_file))
         if File.exist?(include_path)
-          mod_time = File.mtime(include_path)
-          Jekyll.logger.info "Include file mod time: ", mod_time
-          mod_time
+          File.mtime(include_path)
         end
       end
 
@@ -25,7 +21,6 @@ module Jekyll
       # Update last_modified_at to the latest modification time
       latest_mod_time = all_mod_times.max
       self.data['last_modified_at'] = latest_mod_time if latest_mod_time
-      Jekyll.logger.info "Latest mod time: ", latest_mod_time
     end
   end
 
@@ -37,6 +32,14 @@ module Jekyll
         post.update_last_modified_at
         @posts << post
       end
+    end
+  end
+
+  class LastModifiedGenerator < Generator
+    priority :low
+
+    def generate(site)
+      PostCollection.new(site)
     end
   end
 end
