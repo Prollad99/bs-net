6ded2ddad2b8b5ea438c74527b55fe593f059c99
Prollad Mondal <prolladmail@gmail.com>
2024-07-21 09:14:02 +0530

    Update update_last_modified.rb

--- _plugins/update_last_modified.rb
+++ _plugins/update_last_modified.rb
@@ -13,12 +13,14 @@ module Jekyll
       all_mod_times = include_file_mod_times.compact << post_mod_time
       latest_mod_time = all_mod_times.max
 
-      self.data['last_modified_at'] = latest_mod_time if latest_mod_time
+      self.data['last_modified_at'] = latest_mod_time if latest_mod_time && latest_mod_time != self.data['last_modified_at']
     end
   end
 
   Hooks.register :documents, :pre_render do |document|
-    document.extend LastModified
-    document.update_last_modified_at
+    if document.is_a?(Jekyll::Document) && document.collection.label == "posts"
+      document.extend LastModified
+      document.update_last_modified_at
+    end
   end
 end
