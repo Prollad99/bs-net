5da1290f0fe32b57a725392b7de848c2cd4b7a08
Prollad Mondal <prolladmail@gmail.com>
2024-07-22 08:03:49 +0530

    Update update_last_modified.rb

--- _plugins/update_last_modified.rb
+++ _plugins/update_last_modified.rb
@@ -3,29 +3,18 @@ module Jekyll
     priority :low
 
     def generate(site)
-      modified_includes = site.posts.docs.flat_map do |post|
-        includes = post.content.scan(/\{% include (.*?) %\}/).map(&:first).map(&:strip)
-        includes.select do |include_file|
-          include_path = File.join(site.in_source_dir("_includes"), include_file)
-          File.exist?(include_path) && File.mtime(include_path) > File.mtime(post.path)
-        end
-      end.uniq
-
       site.posts.docs.each do |post|
         includes = post.content.scan(/\{% include (.*?) %\}/).map(&:first).map(&:strip)
-        if (includes & modified_includes).any?
-          last_modified = [File.mtime(post.path)] # Initialize with the post's own modification time
+        last_modified = [File.mtime(post.path)] # Start with the post's own modification time
 
-          includes.each do |include_file|
-            include_path = File.join(site.in_source_dir("_includes"), include_file)
-            last_modified << File.mtime(include_path) if File.exist?(include_path)
+        includes.each do |include_file|
+          include_path = File.join(site.in_source_dir("_includes"), include_file)
+          if File.exist?(include_path)
+            last_modified << File.mtime(include_path)
           end
-
-          post.data['last_modified'] = last_modified.max # Update last_modified to the most recent date
         end
 
-        # Debug output
-        puts "Set last_modified for #{post.path} to #{post.data['last_modified']}"
+        post.data['last_modified'] = last_modified.max # Set last_modified to the most recent date
       end
     end
   end
