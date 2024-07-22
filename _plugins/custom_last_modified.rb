3e1b0e13ed13b4c37e782f1580c06d51a82c9871
Prollad Mondal <prolladmail@gmail.com>
2024-07-22 09:31:25 +0530

    Update custom_last_modified.rb

--- _plugins/custom_last_modified.rb
+++ _plugins/custom_last_modified.rb
@@ -7,10 +7,8 @@ module Jekyll
 
     def generate(site)
       site.posts.docs.each do |post|
-        # Update last modified date based on include files
         last_modified = find_last_modified(post, site)
 
-        # Update post's last modified date if missing or if it should be updated
         if post.data['last_modified_at'].nil? || last_modified > Time.parse(post.data['last_modified_at'])
           post.data['last_modified_at'] = last_modified.strftime("%Y-%m-%d %H:%M:%S")
         end
@@ -20,12 +18,24 @@ module Jekyll
     private
 
     def find_last_modified(post, site)
-      last_modified = File.mtime(post.path)
+      last_modified = begin
+        File.mtime(post.path)
+      rescue StandardError => e
+        Jekyll.logger.warn "Error reading mtime for #{post.path}: #{e}"
+        Time.now # fallback to current time if file mtime cannot be read
+      end
 
       post.content.scan(/{% include (.*?) %}/).each do |include_file|
         include_file_path = File.join(site.in_source_dir('_includes', include_file.first))
         if File.exist?(include_file_path)
-          last_modified = [last_modified, File.mtime(include_file_path)].max
+          begin
+            include_mtime = File.mtime(include_file_path)
+            last_modified = [last_modified, include_mtime].max
+          rescue StandardError => e
+            Jekyll.logger.warn "Error reading mtime for include file #{include_file_path}: #{e}"
+          end
+        else
+          Jekyll.logger.warn "Include file not found: #{include_file_path}"
         end
       end
 
