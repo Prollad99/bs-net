d0ad65f50f15ad7f17a604f03f289b22085a1c9e
Prollad Mondal <prolladmail@gmail.com>
2024-07-22 08:47:08 +0530

    Update last_modified.rb

--- _plugins/last_modified.rb
+++ _plugins/last_modified.rb
@@ -6,24 +6,23 @@ module Jekyll
       site.posts.docs.each do |post|
         begin
           includes = post.content.scan(/\{% include (.*?) %\}/).map(&:first).map(&:strip)
-
-          # Start with the post's own modification time
+          
           last_modified = [File.mtime(post.path)]
-          puts "Post: #{post.path}"
-          puts "Initial last_modified: #{last_modified.last}"
+          puts "Processing post: #{post.path}"
+          puts "Post initial last_modified: #{last_modified.last}"
 
           includes.each do |include_file|
             include_path = File.join(site.source, '_includes', include_file)
+            puts "Checking include file: #{include_path}"
             if File.exist?(include_path)
               include_mtime = File.mtime(include_path)
               last_modified << include_mtime
-              puts "Include: #{include_path}, last_modified: #{include_mtime}"
+              puts "Include file: #{include_path} last_modified: #{include_mtime}"
             else
               puts "Include file not found: #{include_path}"
             end
           end
 
-          # Set last_modified to the most recent date
           post.data['last_modified'] = last_modified.max
           puts "Final last_modified for post: #{post.data['last_modified']}"
         rescue => e
