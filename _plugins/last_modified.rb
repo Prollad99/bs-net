e4644242e018df4e940a72e2abbc2916a8de39b6
Prollad Mondal <prolladmail@gmail.com>
2024-07-22 09:02:29 +0530

    Update last_modified.rb

--- _plugins/last_modified.rb
+++ _plugins/last_modified.rb
@@ -1,39 +1,32 @@
+# _plugins/last-modified.rb
+
 module Jekyll
-  class LastModifiedGenerator < Generator
+  class LastModifiedAt < Jekyll::Generator
+    safe true
     priority :low
 
     def generate(site)
       site.posts.docs.each do |post|
-        begin
-          # Start with the post's own modification time
-          last_modified = [File.mtime(post.path)]
-          puts "Processing post: #{post.path} with initial last_modified: #{last_modified.last}"
+        post.data['last_modified_at'] = last_modified_at(post, site)
+      end
+    end
 
-          # Scan for include files in the post content
-          includes = post.content.scan(/\{% include (.*?) %\}/).map(&:first).map(&:strip)
+    private
 
-          includes.each do |include_file|
-            include_path = File.join(site.source, '_includes', include_file)
-            puts "Checking include file: #{include_path}"
+    def last_modified_at(post, site)
+      # Get the last modified time of the post file itself
+      last_modified = File.mtime(post.path)
 
-            if File.exist?(include_path)
-              include_mtime = File.mtime(include_path)
-              last_modified << include_mtime
-              puts "Include file: #{include_path} last_modified: #{include_mtime}"
-            else
-              puts "Include file not found: #{include_path}"
-            end
-          end
-
-          # Set last_modified to the most recent date
-          final_last_modified = last_modified.max
-          post.data['last_modified'] = final_last_modified
-          puts "Final last_modified for post #{post.path}: #{final_last_modified}"
-
-        rescue => e
-          puts "Error processing post #{post.path}: #{e.message}"
+      # Get the last modified time of the include files used in the post
+      post.content.scan(/{% include (.*?) %}/).each do |include_file|
+        include_file_path = File.join(site.in_source_dir('_includes', include_file.first))
+        if File.exist?(include_file_path)
+          last_modified = [last_modified, File.mtime(include_file_path)].max
         end
       end
+
+      # Return the last modified time formatted
+      last_modified.strftime("%Y-%m-%d %H:%M:%S")
     end
   end
 end
