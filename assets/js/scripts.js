916fc8a5fac16b77fa18c9befbeb07f8696c293a
Prollad Mondal <prolladmail@gmail.com>
2024-07-22 19:39:33 +0530

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,7 +2,6 @@ function timeAgo(dateString) {
     const now = new Date();
     const date = new Date(dateString);
 
-    // Convert both dates to UTC
     const nowUTC = Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(),
                             now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
     const dateUTC = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),
@@ -37,6 +36,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const lastUpdatedElement = document.getElementById('last-updated');
     if (lastUpdatedElement) {
         const lastModifiedDate = lastUpdatedElement.getAttribute('data-last-modified');
-        lastUpdatedElement.textContent = timeAgo(lastModifiedDate);
+        const lastModifiedDateUTC = new Date(lastModifiedDate).toISOString();
+        lastUpdatedElement.textContent = timeAgo(lastModifiedDateUTC);
     }
 });
