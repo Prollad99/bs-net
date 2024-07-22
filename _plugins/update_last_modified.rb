2478abbb31c256678aef0a9e3c8788d556172b64
Prollad Mondal <prolladmail@gmail.com>
2024-07-22 07:20:33 +0530

    Update update_last_modified.rb

--- _plugins/update_last_modified.rb
+++ _plugins/update_last_modified.rb
@@ -1,42 +1,21 @@
-require 'jekyll'
-require 'yaml'
-require 'time'
+# Ensure the default external and internal encoding is set to UTF-8
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
 
 module Jekyll
   class LastModifiedGenerator < Generator
-    safe true
+    priority :low
 
     def generate(site)
-      posts_dir = '_posts'
-      Dir.glob("#{posts_dir}/*").each do |file|
-        # Read the file content
-        content = File.read(file)
+      site.posts.docs.each do |post|
+        # Read the content of the file with UTF-8 encoding
+        content = File.read(post.path, encoding: 'UTF-8')
 
-        # Extract the front matter
-        if content =~ /\A---\s*\n(.*?\n?)^---\s*$\n?(.*)/m
-          front_matter = Regexp.last_match(1)
-          body_content = Regexp.last_match(2)
+        # Get the last modified time of the file
+        last_modified = File.mtime(post.path)
 
-          # Parse the front matter YAML
-          data = YAML.load(front_matter)
-
-          # Get the last modified time of the file
-          last_modified_time = File.mtime(file).utc
-
-          # Update the last_modified_at field
-          data['last_modified_at'] = last_modified_time.iso8601
-
-          # Reconstruct the file content
-          new_front_matter = data.to_yaml
-          new_content = "---\n#{new_front_matter}---\n#{body_content}"
-
-          # Write the updated content back to the file
-          File.open(file, 'w') { |f| f.write(new_content) }
-
-          Jekyll.logger.info "Updated last_modified_at in #{file}: #{last_modified_time}"
-        else
-          Jekyll.logger.warn "No front matter found in #{file}, skipping..."
-        end
+        # Assign the last modified time to the post's data
+        post.data['last_modified'] = last_modified
       end
     end
   end
