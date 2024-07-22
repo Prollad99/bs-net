1d0c48f5b96dc16456c149b50031b363320403ce
Prollad Mondal <prolladmail@gmail.com>
2024-07-22 13:40:47 +0530

    Update and rename last_modified_include.rb to ast_modified_tag.rb

---
+++ _plugins/ast_modified_tag.rb
@@ -0,0 +1,22 @@
+module Jekyll
+  class LastModifiedTag < Liquid::Tag
+
+    def initialize(tag_name, text, tokens)
+      super
+      @file_name = text.strip
+    end
+
+    def render(context)
+      site_source = context.registers[:site].source
+      file_path = File.join(site_source, '_includes', @file_name)
+
+      if File.exist?(file_path)
+        File.mtime(file_path).strftime("%Y-%m-%d %H:%M:%S")
+      else
+        "File not found"
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('last_modified', Jekyll::LastModifiedTag)
