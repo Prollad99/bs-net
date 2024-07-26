ca9d6775972bc08eb7ce1de5f5faa1915d83637d
Prollad Mondal <prolladmail@gmail.com>
2024-07-26 21:03:44 +0530

    Update _includes

---
+++ _includes/service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = '{{ page.url | slugify }}-cache-v1';
+const urlsToCache = [
+  '{{ page.url }}',
+  '{{ site.baseurl }}/assets/css/style.css',
+  '{{ site.baseurl }}/assets/js/script.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
