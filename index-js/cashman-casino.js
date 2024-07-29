114bb2fc07bd2ad6d2528bb9b29ba2fd7c624d18
Prollad Mondal <prolladmail@gmail.com>
2024-07-29 19:57:16 +0530

    Update cashman-casino.js

--- index-js/cashman-casino.js
+++ index-js/cashman-casino.js
@@ -11,6 +11,9 @@ axios.get(url)
     const links = [];
 
     $('a[href*="bit.ly"], a[href*="cashman.rocks"], a[href*="cm.onelink.me"]').each((index, element) => {
+if (links.length >= 100) {
+        return false; // Break out of the loop if we have 100 links
+      }
       const link = $(element).attr('href');
       const text = $(element).text().trim();
       links.push({ href: link, text: text });
