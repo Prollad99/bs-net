7dc21504ee20927f5f0ca684de495c80707a4098
Prollad Mondal <prolladmail@gmail.com>
2024-07-29 19:58:38 +0530

    Update hit-it-rich.js

--- index-js/hit-it-rich.js
+++ index-js/hit-it-rich.js
@@ -11,6 +11,9 @@ axios.get(url)
     const links = [];
 
     $('a[href*="web.hititrich.zynga.com"], a[href*="zynga.social"]').each((index, element) => {
+if (links.length >= 100) {
+        return false; // Break out of the loop if we have 100 links
+      }
       const link = $(element).attr('href');
       const text = $(element).text().trim();
       links.push({ href: link, text: text });
