eb0bbef13c0f3de04b3a50c7eeb2fc1df3e9bc48
Prollad Mondal <prolladmail@gmail.com>
2024-07-30 14:15:27 +0530

    Update index-js

---
+++ index-js/lotsa-slots.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const path = require('path');
+
+const url = 'https://mosttechs.com/lotsa-slots-free-coins/';
+
+axios.get(url)
+  .then(({ data }) => {
+    const $ = cheerio.load(data);
+    const links = [];
+
+    $('a[href*="bit.ly"]').each((index, element) => {
+if (links.length >= 100) {
+        return false; // Break out of the loop if we have 100 links
+      }
+      const link = $(element).attr('href');
+      const text = $(element).text().trim();
+      links.push({ href: link, text: text });
+    });
+
+    console.log('Fetched links:', links);
+
+    const dir = 'links-json';
+    if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+    }
+
+    const filePath = path.join(dir, 'lotsa-slots.json');
+    fs.writeFileSync(filePath, JSON.stringify(links, null, 2), 'utf8');
+    console.log(`Links saved to ${filePath}`);
+  })
+  .catch(err => {
+    console.error('Error fetching links:', err);
+    process.exit(1);
+  });
