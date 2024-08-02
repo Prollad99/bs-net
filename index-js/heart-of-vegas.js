e544478d58746d515de280e3aff210d061b9e7b8
Prollad Mondal <prolladmail@gmail.com>
2024-08-02 10:17:25 +0530

    Update index-js

---
+++ index-js/heart-of-vegas.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const path = require('path');
+
+const url = 'https://mosttechs.com/heart-of-vegas-free-coins/';
+
+axios.get(url)
+  .then(({ data }) => {
+    const $ = cheerio.load(data);
+    const links = [];
+
+    $('a[href*="hovsocial.onelink.me"], a[href*="bit.ly"], a[href*="hov.rocks"]').each((index, element) => {
+if (links.length >= 100) {
+        return false; // Break out of the loop if we have 100 links
+      }
+      const link = $(element).attr('href');
+      const text = $(element).text().trim();
+      links.push({ href: link, text: text });
+    });
+
+    console.log('Fetched links:', links);
+
+    const dir = 'links-json';
+    if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+    }
+
+    const filePath = path.join(dir, 'heart-of-vegas.json');
+    fs.writeFileSync(filePath, JSON.stringify(links, null, 2), 'utf8');
+    console.log(`Links saved to ${filePath}`);
+  })
+  .catch(err => {
+    console.error('Error fetching links:', err);
+    process.exit(1);
+  });
