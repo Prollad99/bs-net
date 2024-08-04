586a32c12f51ddab4efd758718adbc276172ca20
Prollad Mondal <prolladmail@gmail.com>
2024-08-04 08:53:25 +0530

    Update bingo-bash.js

--- index-js/bingo-bash.js
+++ index-js/bingo-bash.js
@@ -12,14 +12,9 @@ function getCurrentDate() {
   return `${year}-${month}-${day}`;
 }
 
-// Function to format the date in "Month Day, Year" format or replace invalid dates with the current year
+// Function to format the date in "Month Day, Year" format
 function formatDate(dateString) {
   const date = new Date(dateString);
-  if (isNaN(date)) {
-    // Return the current year if the date is invalid
-    const currentYear = new Date().getFullYear();
-    return `Year ${currentYear}`;
-  }
   return date.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
 }
 
@@ -75,9 +70,8 @@ async function main() {
     // Generate HTML file
     let htmlContent = '<ul class="list-group mt-3 mb-4">\n';
     combinedLinks.forEach(link => {
-      const formattedDate = formatDate(link.date);
       htmlContent += `  <li class="list-group-item d-flex justify-content-between align-items-center">\n`;
-      htmlContent += `    <span>Free Chips for ${formattedDate}</span>\n`;
+      htmlContent += `    <span>Free Chips for ${formatDate(link.date)}</span>\n`;
       htmlContent += `    <a href="${link.href}" class="btn btn-primary btn-sm">Collect</a>\n`;
       htmlContent += `  </li>\n`;
     });
