f1fdc21291682d9b20b99c128e0b7bbdfd07d97e
Prollad Mondal <prolladmail@gmail.com>
2024-08-06 20:01:11 +0530

    Update island-king.js

--- index-js/island-king.js
+++ index-js/island-king.js
@@ -18,7 +18,7 @@ function formatDate(dateString) {
   return date.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
 }
 
-const url = 'https://www.crazyashwin.com/island-king-free-spin/';
+const url = 'https://mosttechs.com/island-king-free-spins-and-coins/';
 const currentDate = getCurrentDate();
 const dir = 'links-json';
 const filePath = path.join(dir, 'island-king.json');
