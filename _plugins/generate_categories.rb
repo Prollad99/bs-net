92354a21afa9ed6a3ad7e78e7b38f74baf113e7d
Prollad Mondal <prolladmail@gmail.com>
2024-08-10 15:58:03 +0530

    Update generate_categories.rb

--- _plugins/generate_categories.rb
+++ _plugins/generate_categories.rb
@@ -5,23 +5,23 @@ module Jekyll
     def generate(site)
       if site.layouts.key? 'category'
         site.categories.each do |category, posts|
-          site.pages << CategoryPage.new(site, site.source, category)
+          site.pages << CategoryPage.new(site, site.source, File.join('', category), category)
         end
       end
     end
   end
 
   class CategoryPage < Page
-    def initialize(site, base, category)
+    def initialize(site, base, dir, category)
       @site = site
       @base = base
-      @dir  = category
+      @dir  = dir
       @name = 'index.html'
 
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'category.html')
       self.data['category'] = category
-      self.data['title'] = "Posts in Category \"#{category}\""
+      self.data['title'] = "Posts in #{category.capitalize}"
     end
   end
 end
