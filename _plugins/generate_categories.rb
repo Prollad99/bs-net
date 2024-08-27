827d8290c5f5dfc488242eaba6dca954414c498f
Prollad Mondal <prolladmail@gmail.com>
2024-08-27 12:53:04 +0530

    Update generate_categories.rb

--- _plugins/generate_categories.rb
+++ _plugins/generate_categories.rb
@@ -4,7 +4,13 @@ module Jekyll
 
     def generate(site)
       if site.layouts.key? 'category'
+        # Get a list of language directories
+        language_dirs = site.config['languages'] || []
+
         site.categories.each do |category, posts|
+          # Skip language directories by checking against language_dirs list
+          next if language_dirs.include?(category)
+          
           site.pages << CategoryPage.new(site, site.source, category)
         end
       end
@@ -15,13 +21,13 @@ module Jekyll
     def initialize(site, base, category)
       @site = site
       @base = base
-      @dir  = category
+      @dir  = File.join('category', category)
       @name = 'index.html'
 
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'category.html')
       self.data['category'] = category
-      
+
       # Convert category slug to title case and format the title
       formatted_category = category.split('-').map(&:capitalize).join(' ')
       self.data['title'] = "#{formatted_category}: What You Need to Know"
